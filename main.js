4875b30235b521fed39f8f757bae4d40b8e003a1
santiago Álvarez <santiagoaas10@gmail.com>
2023-12-27 00:17:51 -0500

    progreso hasta la clase 4, invocación de API por medio de funciones asíncronas y creación de botón para cambiar las imágenes de los gaticos

---
+++ main.js
@@ -0,0 +1,24 @@
+console.log('Hello, world')
+
+const API_URL = 'https://api.thecatapi.com/v1/images/search';
+
+
+
+ async function reload(){
+  const res= await fetch(API_URL)
+  const data = await res.json()
+  const img = document.querySelector('img');
+  img.src = data[0].url;
+    }
+/*   .then(res => res.json())
+  .then(datos => {
+    const img = document.querySelector('img');
+    img.src = data[0].url;
+    console.log(data)
+   
+  }); */
+
+  reload();
+
+
+  
