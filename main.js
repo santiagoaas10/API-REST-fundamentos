7edf153bc72c722895bc185a6a2ea2e152f52264
santiago Álvarez <santiagoaas10@gmail.com>
2023-12-29 22:04:29 -0500

    clase 9: método get, y manejo de errores

--- main.js
+++ main.js
@@ -1,30 +1,35 @@
-console.log('Hello, world')
+const API_URL_RANDOM = 'https://api.thecatapi.com/v1/images/search?limit=2&api_key=c08d415f-dea7-4a38-bb28-7b2188202e46';
+const API_URL_FAVOTITES = 'https://api.thecatapi.com/v1/favourites?limit=2&api_key=c08d415f-dea7-4a38-bb28-7b2188202e46';
 
-const API_URL = 'https://api.thecatapi.com/v1/images/search?limit=20&api_key=live_23ivvPfP3gQTC6uAbbu8ocGphLEnOVoxlKQQ41ZyAYBabzD8Jd7I2b2NeEdNPCT9';
+const spanError = document.getElementById('error')
 
-
- async function reload(){
-  const res= await fetch(API_URL)
-  const data = await res.json()
-
+async function loadRandomMichis() {
+  const res = await fetch(API_URL_RANDOM);
+  const data = await res.json();
+  console.log('Random')
   console.log(data)
-  const img1 = document.getElementById('img1');
-  const img2 = document.getElementById('img2');
-  const img3 = document.getElementById('img3');
-  img1.src = data[0].url;
-  img2.src = data[1].url;
-  img3.src = data[2].url;
 
-    }
-/*   .then(res => res.json())
-  .then(datos => {
-    const img = document.querySelector('img');
-    img.src = data[0].url;
-    console.log(data)
-   
-  }); */
+  if (res.status !== 200) {
+    spanError.innerHTML = "Hubo un error: " + res.status;
+  } else {
+    const img1 = document.getElementById('img1');
+    const img2 = document.getElementById('img2');
+    
+    img1.src = data[0].url;
+    img2.src = data[1].url;
+  }
+}
 
-  reload();
+async function loadFavoritesMichis() {
+  const res = await fetch(API_URL_FAVOTITES);
+  const data = await res.json();
+  console.log('Favoritos')
+  console.log(data)
 
+  if (res.status !== 200) {
+    spanError.innerHTML = "Hubo un error: " + res.status + data.message;
+  }
+}
 
-  
+loadRandomMichis();
+loadFavoritesMichis();
